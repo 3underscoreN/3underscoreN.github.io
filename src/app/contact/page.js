2d7c4389bdaa300f255d48170f7c65870a6ca950
Kelvin <thisisrandom838@gmail.com>
2025-03-06 10:14:18 +0800

    fix: insecuirt issue in /contact (hopefully)

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,6 +6,13 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faLinkedin, faGithub, faInstagram, faFacebook, faDiscord } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
+    const openURL = (url) => {
+        return (() => {
+            window.open(url);
+        });
+    }
+
+
     return (
         <>
             <div className="my-10 mx-10">
@@ -20,24 +27,18 @@ const Contact = () => {
                         </div>
                         <div className="my-5" />
                         <div className="flex flex-col md:flex-row justify-center place-items-center md:justify-start gap-5">
-                            <form action="">
-                                <button className="btn btn-accent dark:btn-primary">
+                                <button className="btn btn-accent dark:btn-primary" onClick={openURL("mailto:cychandt@connect.ust.hk")}>
                                     <FontAwesomeIcon icon={faEnvelope} />
                                     <p>cychandt@connect.ust.hk</p>
                                 </button>
-                            </form>
-                            <form action="https://www.linkedin.com/in/chung-yuk-chan-6a2742241/">
-                                <button className="btn btn-accent dark:btn-primary">
+                                <button className="btn btn-accent dark:btn-primary" onClick={openURL("https://www.linkedin.com/in/chung-yuk-chan-6a2742241/")}>
                                     <FontAwesomeIcon icon={faLinkedin} />
                                     <p>LinkedIn</p>
                                 </button>
-                            </form>
-                            <form action="https://github.com/3underscoreN">
-                                <button className="btn btn-accent dark:btn-primary">
+                                <button className="btn btn-accent dark:btn-primary" onClick={openURL("https://github.com/3underscoreN")}>
                                     <FontAwesomeIcon icon={faGithub} />
                                     <p>GitHub</p>
                                 </button>
-                            </form>
                         </div>
                     </div>
                     <div className="w-full md:w-auto">
@@ -46,18 +47,14 @@ const Contact = () => {
                         </div>
                         <div className="my-5" />
                         <div className="flex flex-col md:flex-row justify-center place-items-center md:justify-start gap-5">
-                            <form action="https://www.instagram.com/f.prime.of.x/">
-                                <button className="btn btn-accent dark:btn-primary">
+                                <button className="btn btn-accent dark:btn-primary" onClick={openURL("https://www.instagram.com/f.prime.of.x/")}>
                                     <FontAwesomeIcon icon={faInstagram} />
                                     <p>Instagram</p>
                                 </button>
-                            </form>
-                            <form action="https://www.facebook.com/chungyuk.chan.5">
-                                <button className="btn btn-accent dark:btn-primary">
+                                <button className="btn btn-accent dark:btn-primary" onClick={openURL("https://www.facebook.com/chungyuk.chan.5")}>
                                     <FontAwesomeIcon icon={faFacebook} />
                                     <p>Facebook</p>
                                 </button>
-                            </form>
                             <div className="tooltip" data-tip="Only if I know you well enough :)">
                                 <button className="btn btn-warning btn-outline">
                                     <FontAwesomeIcon icon={faDiscord} />
