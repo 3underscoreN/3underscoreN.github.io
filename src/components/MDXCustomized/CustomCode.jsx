75cfade514a9a6adede2c9ec726fc4b02893c28b
Kelvin <thisisrandom838@gmail.com>
2026-04-25 23:22:59 +0800

    feat: add language details and copy function

--- src/components/MDXCustomized/CustomCode.jsx
+++ src/components/MDXCustomized/CustomCode.jsx
@@ -1,7 +1,18 @@
+"use client";
+
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import { cn } from "@/util/cn";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCode,
+  faClipboard,
+  faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { useState } from "react";
 
 import { Fira_Code } from "next/font/google";
 const firacode = Fira_Code({
@@ -23,6 +34,14 @@ export function CustomInlineCode({ children, className }) {
 }
 
 export function CustomMultilineCode({ children }) {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = (text) => {
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+  };
+
   return (
     <div
       className={cn(
@@ -30,6 +49,24 @@ export function CustomMultilineCode({ children }) {
         firacode.className,
       )}
     >
+      <div className="mt-2 flex flex-row items-center justify-start gap-2">
+        <FontAwesomeIcon icon={faCode} />
+        <span>{children.props.className?.replace("language-", "")}</span>
+        <div className="flex-1" />
+        <button
+          className={cn(
+            "btn btn-sm btn-icon btn-circle",
+            copied ? "btn-success" : "",
+          )}
+          onClick={() => handleCopy(children.props.children)}
+        >
+          {copied ? (
+            <FontAwesomeIcon icon={faCheck} />
+          ) : (
+            <FontAwesomeIcon icon={faClipboard} />
+          )}
+        </button>
+      </div>
       <SyntaxHighlighter
         style={atomDark}
         language={children.props.className?.replace("language-", "")}
