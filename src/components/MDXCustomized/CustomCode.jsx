57bd108066d2360200a9941bd17ffc05af1c87d6
Kelvin <me@3underscoreN.work>
2026-09-14 14:59:21 +0800

    fix: handle potential undefined props in CustomMultilineCode component

--- src/components/MDXCustomized/CustomCode.jsx
+++ src/components/MDXCustomized/CustomCode.jsx
@@ -51,7 +51,7 @@ export function CustomMultilineCode({ children }) {
     >
       <div className="mt-2 flex flex-row items-center justify-start gap-2 text-neutral-content dark:text-base-content">
         <FontAwesomeIcon icon={faCode} />
-        <span>{children.props.className?.replace("language-", "")}</span>
+        <span>{children.props ? children.props.className?.replace("language-", "") : ""}</span>
         <div className="flex-1" />
         <button
           className={cn(
